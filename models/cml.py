e60578f6ca04ca6ab3886bf30e5b8468330fd12d
wuga <wuga214@gmail.com>
2018-10-30 18:11:14 -0400

    Upload Exist Models

---
+++ models/cml.py
@@ -0,0 +1,134 @@
+import tensorflow as tf
+import numpy as np
+from tqdm import tqdm
+from scipy.sparse import lil_matrix
+import pyximport; pyximport.install()
+from utils.cython.negative_sampler import get_negative_samples
+
+
+class CollaborativeMetricLearning(object):
+    def __init__(self,
+                 num_users,
+                 num_items,
+                 embed_dim,
+                 batch_size=10000,
+                 margin=1.0,
+                 clip_norm=1.0,
+                 cov_loss_weight=0.01,
+                 **unused):
+        self.num_users = num_users
+        self.num_items = num_items
+        self.embed_dim = embed_dim
+        self.batch_size = batch_size
+        self.margin = margin
+        self.clip_norm = clip_norm
+        self.cov_loss_weight = cov_loss_weight
+        self.get_graph()
+        self.sess = tf.Session()
+        self.sess.run(tf.global_variables_initializer())
+
+    def get_graph(self):
+
+        # Placehoders
+
+        # M users
+        self.pos_sample_idx = tf.placeholder(tf.int32, [None])
+
+        # M positive items
+        self.neg_sample_idx = tf.placeholder(tf.int32, [None, None])
+
+        # M X N positive items
+        self.user_idx = tf.placeholder(tf.int32, [None])
+
+        # Variable to learn
+        self.user_embeddings = tf.Variable(tf.random_normal([self.num_users, self.embed_dim],
+                                                            stddev=1 / (self.embed_dim ** 0.5), dtype=tf.float32))
+
+        self.item_embeddings = tf.Variable(tf.random_normal([self.num_items, self.embed_dim],
+                                                            stddev=1 / (self.embed_dim ** 0.5), dtype=tf.float32))
+
+        with tf.variable_scope("covariance_loss"):
+            embedding = tf.concat((self.item_embeddings, self.user_embeddings), 0)
+            n_rows = tf.cast(tf.shape(embedding)[0], tf.float32)
+            X = embedding - (tf.reduce_mean(embedding, axis=0))
+            cov = tf.matmul(X, X, transpose_a=True) / n_rows
+            cov_loss = tf.reduce_sum(tf.matrix_set_diag(cov, tf.zeros(self.embed_dim, tf.float32))
+                                     ) * self.cov_loss_weight
+
+        with tf.variable_scope("metric_loss"):
+            users = tf.nn.embedding_lookup(self.user_embeddings, self.user_idx, name="users")
+            pos_samples = tf.nn.embedding_lookup(self.item_embeddings, self.pos_sample_idx, name="pos_items")
+            neg_samples = tf.transpose(tf.nn.embedding_lookup(self.item_embeddings, self.neg_sample_idx),
+                                       (0, 2, 1), name="neg_items")
+            pos_distances = tf.reduce_sum(tf.squared_difference(users, pos_samples),
+                                          axis=1,
+                                          name="pos_distances")
+            neg_distances = tf.reduce_sum(tf.squared_difference(tf.expand_dims(users, -1), neg_samples),
+                                          axis=1,
+                                          name="neg_distances")
+            shortest_neg_distances = tf.reduce_min(neg_distances, 1, name="shortest_neg_distances")
+            hinge_loss = tf.maximum(pos_distances - shortest_neg_distances + self.margin, 0, name="pair_loss")
+            impostors = (tf.expand_dims(pos_distances, -1) - neg_distances + self.margin) > 0
+            rank = tf.reduce_mean(tf.cast(impostors, dtype=tf.float32), 1, name="rank_weight") * self.num_items
+            metric_loss = hinge_loss * tf.log(rank + 1)
+
+        self.loss = cov_loss + metric_loss
+
+        with tf.variable_scope("optimizer"):
+            self.optimizer = tf.train.AdamOptimizer().minimize(self.loss,
+                                                               var_list=[self.user_embeddings,
+                                                                         self.item_embeddings])
+
+        with tf.variable_scope("clip"):
+            self.clips = [
+                tf.assign(self.user_embeddings, tf.clip_by_norm(self.user_embeddings, self.clip_norm, axes=[1])),
+                tf.assign(self.item_embeddings, tf.clip_by_norm(self.item_embeddings, self.clip_norm, axes=[1]))
+            ]
+
+    def train_model(self, rating_matrix, epoch=100):
+
+        user_item_matrix = lil_matrix(rating_matrix)
+        user_item_pairs = np.asarray(user_item_matrix.nonzero()).T
+        user_to_positive_set = {u: set(row) for u, row in enumerate(user_item_matrix.rows)}
+
+        summary_writer = tf.summary.FileWriter('cml', graph=self.sess.graph)
+
+        # Training
+        for i in range(epoch):
+            batches = self.get_batches(user_item_pairs, user_to_positive_set,
+                                       user_item_matrix.shape[1], self.batch_size, 10)
+            for step in tqdm(range(len(batches))):
+                feed_dict = {self.user_idx: batches[step][0],
+                             self.pos_sample_idx: batches[step][1],
+                             self.neg_sample_idx: batches[step][2]
+                             }
+                training = self.sess.run([self.optimizer], feed_dict=feed_dict)
+                clip = self.sess.run(self.clips)
+
+    @staticmethod
+    def get_batches(user_item_pairs, user_to_positive_set, num_item, batch_size, n_negative):
+        batches = []
+
+        index_shuf = range(len(user_item_pairs))
+        np.random.shuffle(index_shuf)
+        user_item_pairs = user_item_pairs[index_shuf]
+        for i in tqdm(range(int(len(user_item_pairs) / batch_size))):
+
+            ui_pairs = user_item_pairs[i * batch_size: (i + 1) * batch_size, :]
+
+            negative_samples = np.random.randint(
+                0,
+                num_item,
+                size=(batch_size, n_negative))
+
+            # for user_positive, negatives, i in zip(ui_pairs,
+            #                                        negative_samples,
+            #                                        range(len(negative_samples))):
+            #     user = user_positive[0]
+            #     for j, neg in enumerate(negatives):
+            #         while neg in user_to_positive_set[user]:
+            #             negative_samples[i, j] = neg = np.random.randint(0, num_item)
+            negative_samples = get_negative_samples(user_to_positive_set, ui_pairs[:, 0], num_item, negative_samples)
+            batches.append([ui_pairs[:, 0], ui_pairs[:, 1], negative_samples])
+
+        return batches
---
+++ models/vae.py
@@ -0,0 +1,126 @@
+import re
+import numpy as np
+import tensorflow as tf
+from tensorflow.contrib.distributions import Bernoulli
+
+
+class VAE(object):
+
+    def __init__(self, latent_dim, batch_size, encoder, decoder,
+                 observation_dim=784,
+                 learning_rate=1e-4,
+                 optimizer=tf.train.RMSPropOptimizer,
+                 observation_distribution="Bernoulli", # or Gaussian
+                 observation_std=0.01):
+
+        self._latent_dim = latent_dim
+        self._batch_size = batch_size
+        self._encode = encoder
+        self._decode = decoder
+        self._observation_dim = observation_dim
+        self._learning_rate = learning_rate
+        self._optimizer = optimizer
+        self._observation_distribution = observation_distribution
+        self._observation_std = observation_std
+        self._build_graph()
+
+    def _build_graph(self):
+
+        with tf.variable_scope('vae'):
+            self.x = tf.placeholder(tf.float32, shape=[None, self._observation_dim])
+
+            with tf.variable_scope('encoder'):
+                encoded = self._encode(self.x, self._latent_dim)
+
+            with tf.variable_scope('latent'):
+                self.mean = encoded[:, :self._latent_dim]
+                logvar = encoded[:, self._latent_dim:]
+                stddev = tf.sqrt(tf.exp(logvar))
+                epsilon = tf.random_normal([self._batch_size, self._latent_dim])
+                self.z = self.mean + stddev * epsilon
+
+            with tf.variable_scope('decoder'):
+                decoded = self._decode(self.z, self._observation_dim)
+                self.obs_mean = decoded
+                if self._observation_distribution == 'Gaussian':
+                    obs_epsilon = tf.random_normal([self._batch_size, self._observation_dim])
+                    self.sample = self.obs_mean + self._observation_std * obs_epsilon
+                else:
+                    self.sample = Bernoulli(probs=self.obs_mean).sample()
+
+
+            with tf.variable_scope('loss'):
+                with tf.variable_scope('kl-divergence'):
+                    kl = self._kl_diagnormal_stdnormal(self.mean, logvar)
+
+                if self._observation_distribution == 'Gaussian':
+                    with tf.variable_scope('gaussian'):
+                        obj = self._gaussian_log_likelihood(self.x, self.obs_mean, self._observation_std)
+                else:
+                    with tf.variable_scope('bernoulli'):
+                        obj = self._bernoulli_log_likelihood(self.x, self.obs_mean)
+
+                self._loss = (kl + obj) / self._batch_size
+
+            with tf.variable_scope('optimizer'):
+                optimizer = tf.train.RMSPropOptimizer(learning_rate=self._learning_rate)
+            with tf.variable_scope('training-step'):
+                self._train = optimizer.minimize(self._loss)
+
+            self._sesh = tf.Session()
+            init = tf.global_variables_initializer()
+            self._sesh.run(init)
+
+    @staticmethod
+    def _kl_diagnormal_stdnormal(mu, log_var):
+
+        var = tf.exp(log_var)
+        kl = 0.5 * tf.reduce_sum(tf.square(mu) + var - 1. - log_var)
+        return kl
+
+    @staticmethod
+    def _gaussian_log_likelihood(targets, mean, std):
+        se = 0.5 * tf.reduce_sum(tf.square(targets - mean)) / (2*tf.square(std)) + tf.log(std)
+        return se
+
+    @staticmethod
+    def _bernoulli_log_likelihood(targets, outputs, eps=1e-8):
+
+        log_like = -tf.reduce_sum(targets * tf.log(outputs + eps)
+                                  + (1. - targets) * tf.log((1. - outputs) + eps))
+        return log_like
+
+    def update(self, x):
+        _, loss = self._sesh.run([self._train, self._loss], feed_dict={self.x: x})
+        return loss
+
+    def x2z(self, x):
+
+        mean = self._sesh.run([self.mean], feed_dict={self.x: x})
+
+        return np.asarray(mean).reshape(-1, self._latent_dim)
+
+    def z2x(self, z):
+
+        x = self._sesh.run([self.obs_mean], feed_dict={self.z: z})
+        return x
+
+    def sample(self, z):
+
+        x = self._sesh.run([self.sample], feed_dict={self.z: z})
+
+        return x
+
+    def save_generator(self, path, prefix="in/generator"):
+        variables = tf.trainable_variables()
+        var_dict = {}
+        for v in variables:
+            if "decoder" in v.name:
+                name = prefix+re.sub("vae/decoder", "", v.name)
+                name = re.sub(":0", "", name)
+                var_dict[name] = v
+        for k, v in var_dict.items():
+            print(k)
+            print(v)
+        saver = tf.train.Saver(var_dict)
+        saver.save(self._sesh, path)
