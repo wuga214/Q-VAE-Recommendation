ef84afedee74028a196f9c2c22452bd9d1abe840
wuga <wuga214@gmail.com>
2018-11-28 13:42:14 -0500

    Add Movielens 1m experiment

---
+++ experiment/tuning.py
@@ -0,0 +1,56 @@
+import numpy as np
+import pandas as pd
+from tqdm import tqdm
+from evaluation.metrics import evaluate
+from utils.progress import WorkSplitter
+import inspect
+from models.predictor import predict
+
+
+def hyper_parameter_tuning(train, validation, params, measure='Cosine', gpu_on=True):
+    progress = WorkSplitter()
+    df = pd.DataFrame(columns=['model', 'rank', 'alpha', 'root', 'topK'])
+
+    num_user = train.shape[0]
+
+    for algorithm in params['models']:
+
+        for rank in params['rank']:
+            if 'alpha' in inspect.getargspec(params['models'][algorithm])[0]:
+                alphas = params['alpha']
+            else:
+                alphas = [1]
+
+            for alpha in alphas:
+
+                for lam in params['lam']:
+
+                    progress.section("model: {0}, rank: {1}, alpha: {2}, lambda: {3}".format(algorithm,
+                                                                                             rank,
+                                                                                             alpha,
+                                                                                             lam))
+                    RQ, Yt, Bias = params['models'][algorithm](train,
+                                                               embeded_matrix=np.empty((0)),
+                                                               iteration=params['iter'],
+                                                               rank=rank,
+                                                               lam=lam,
+                                                               alpha=alpha,
+                                                               gpu_on=True)
+                    Y = Yt.T
+
+                    progress.subsection("Prediction")
+
+                    prediction = predict(matrix_U=RQ, matrix_V=Y, measure=measure, bias=Bias,
+                                         topK=params['topK'][-1], matrix_Train=train, gpu=gpu_on)
+
+                    progress.subsection("Evaluation")
+
+                    result = evaluate(prediction, validation, params['metric'], params['topK'])
+
+                    result_dict = {'model': algorithm, 'rank': rank, 'alpha': alpha, 'lambda': lam}
+
+                    for name in result.keys():
+                        result_dict[name] = [round(result[name][0], 4), round(result[name][1], 4)]
+
+                    df = df.append(result_dict, ignore_index=True)
+    return df
---
+++ tune_parameters.py
@@ -0,0 +1,46 @@
+import numpy as np
+import argparse
+from models.cdae import cdae
+from models.vae import vae_cf
+from models.ifvae import ifvae
+from models.autorec import autorec
+from experiment.tuning import hyper_parameter_tuning
+from utils.io import load_numpy, save_dataframe_latex, save_dataframe_csv
+
+
+models = {
+    "AutoRec": autorec,
+    "CDAE": cdae,
+    "VAE-CF": vae_cf,
+    "IFVAE": ifvae
+}
+
+
+def main(args):
+    params = {
+        'models': models,
+        'alpha': [1],
+        'rank': [50, 100],
+        'root': [1.0],
+        'topK': [5, 10, 15, 20, 50],
+        'iter': 300,
+        'lam': [1e-6, 1e-5, 1e-4, 1e-3, 1e-2, 0.1, 1],
+        'metric': ['R-Precision', 'NDCG', 'Precision', 'Recall'],
+    }
+
+    R_train = load_numpy(path=args.path, name=args.train)
+    R_valid = load_numpy(path=args.path, name=args.valid)
+    df = hyper_parameter_tuning(R_train, R_valid, params, measure="Cosine")
+    save_dataframe_latex(df, 'tables/', args.name)
+    save_dataframe_csv(df, 'tables/', args.name)
+
+if __name__ == "__main__":
+    # Commandline arguments
+    parser = argparse.ArgumentParser(description="LRec")
+    parser.add_argument('-n', dest='name', default="autorecs_tuning")
+    parser.add_argument('-d', dest='path', default="datax/")
+    parser.add_argument('-t', dest='train', default='Rtrain.npz')
+    parser.add_argument('-v', dest='valid', default='Rvalid.npz')
+    args = parser.parse_args()
+
+    main(args)
