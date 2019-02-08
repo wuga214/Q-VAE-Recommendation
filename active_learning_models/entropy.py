1be55dcf69a85f3a633a96e7249d12f721ac2410
k9luo <lucas.luokai@gmail.com>
2019-02-08 17:13:57 -0500

    entropy wip

--- active_learning_models/entropy.py
+++ active_learning_models/entropy.py
@@ -22,7 +22,7 @@ class Entropy(object):
         one_minus_p_log2_one_minus_p = np.multiply(1-normalized_pdf, 1-np.log2(normalized_pdf))
         return np.negative(np.nan_to_num(p_log2_p) + np.nan_to_num(one_minus_p_log2_one_minus_p))
 
-    def eval(self, prediction_scores, matrix_input, matrix_valid, topk, gpu_on):
+    def eval(self, prediction_scores, matrix_input, matrix_test, topk, gpu_on):
         start_time = time.time()
 
         metric_names = ['R-Precision', 'NDCG', 'Clicks', 'Recall', 'Precision', 'MAP']
@@ -31,7 +31,7 @@ class Entropy(object):
                                             topK=topk,
                                             matrix_train=matrix_input,
                                             gpu=gpu_on)
-        result = evaluate(evaluation_items, matrix_valid, metric_names, [topk])
+        result = evaluate(evaluation_items, matrix_test, metric_names, [topk])
 
         print("-")
         for metric in result.keys():
@@ -40,54 +40,54 @@ class Entropy(object):
 
         return result
 
-    def update_matrix(self, prediction, matrix_valid, result, matrix_input, matrix_input_with_negative):
+    def update_matrix(self, prediction, matrix_test, result, matrix_input, matrix_input_with_negative):
         start_time = time.time()
-        # Move these ‘k’ samples from the validation set to the train-set
+        # Move these ‘k’ samples from the test set to the train-set
         # and query their labels.
         index = np.tile(np.arange(prediction.shape[0]),(prediction.shape[1],1)).T
         index_prediction = np.dstack((index, prediction)).reshape((prediction.shape[0]*prediction.shape[1]), 2)
-        index_valid_nonzero = np.dstack((matrix_valid.nonzero()[0], matrix_valid.nonzero()[1]))[0]
+        index_test_ones = np.dstack((matrix_test.nonzero()[0], matrix_test.nonzero()[1]))[0]
 
         index_prediction_set = set([tuple(x) for x in index_prediction])
-        index_valid_nonzero_set = set([tuple(x) for x in index_valid_nonzero])
-        prediction_valid_nonzero_intersect = np.array([x for x in index_prediction_set & index_valid_nonzero_set])
-        print('The number of unmasked positive data is {}'.format(len(prediction_valid_nonzero_intersect)))
-        prediction_valid_zero_intersect = np.array([x for x in index_prediction_set - index_valid_nonzero_set])
-        print('The number of unmasked negative data is {}'.format(len(prediction_valid_zero_intersect)))
+        index_test_ones_set = set([tuple(x) for x in index_test_ones])
+        prediction_test_ones_intersect = np.array([x for x in index_prediction_set & index_test_ones_set])
+        print('The number of unmasked positive data is {}'.format(len(prediction_test_ones_intersect)))
+        prediction_test_zero_intersect = np.array([x for x in index_prediction_set - index_test_ones_set])
+        print('The number of unmasked negative data is {}'.format(len(prediction_test_zero_intersect)))
 
         result['Num_Nonzero_In_Train'] = len(matrix_input.nonzero()[0])
-        result['Num_Nonzero_In_Valid'] = len(matrix_valid.nonzero()[0])
-        result['Num_Unmasked_Positive'] = len(prediction_valid_nonzero_intersect)
-        result['Num_Unmasked_Negative'] = len(prediction_valid_zero_intersect)
+        result['Num_Nonzero_In_Valid'] = len(matrix_test.nonzero()[0])
+        result['Num_Unmasked_Positive'] = len(prediction_test_ones_intersect)
+        result['Num_Unmasked_Negative'] = len(prediction_test_zero_intersect)
 
-        if len(prediction_valid_nonzero_intersect) > 0:
-            mask_row = prediction_valid_nonzero_intersect[:, 0]
-            mask_col = prediction_valid_nonzero_intersect[:, 1]
-            mask_data = np.full(len(prediction_valid_nonzero_intersect), True)
+        if len(prediction_test_ones_intersect) > 0:
+            mask_row = prediction_test_ones_intersect[:, 0]
+            mask_col = prediction_test_ones_intersect[:, 1]
+            mask_data = np.full(len(prediction_test_ones_intersect), True)
             mask = csr_matrix((mask_data, (mask_row, mask_col)), shape=matrix_input.shape)
 
             matrix_input = matrix_input.tolil()
             matrix_input_with_negative = matrix_input_with_negative.tolil()
-            matrix_valid = matrix_valid.tolil()
+            matrix_test = matrix_test.tolil()
 
             matrix_input[mask] = 1
-            matrix_valid[mask] = 0
+            matrix_test[mask] = 0
             matrix_input_with_negative[mask] = 1
 
-        if len(prediction_valid_zero_intersect) > 0:
-            mask_row = prediction_valid_zero_intersect[:, 0]
-            mask_col = prediction_valid_zero_intersect[:, 1]
-            mask_data = np.full(len(prediction_valid_zero_intersect), True)
+        if len(prediction_test_zero_intersect) > 0:
+            mask_row = prediction_test_zero_intersect[:, 0]
+            mask_col = prediction_test_zero_intersect[:, 1]
+            mask_data = np.full(len(prediction_test_zero_intersect), True)
             mask = csr_matrix((mask_data, (mask_row, mask_col)), shape=matrix_input.shape)
 
             matrix_input_with_negative[mask] = -0.1
 
         print("Elapsed: {0}".format(inhour(time.time() - start_time)))
 
-        return result, matrix_input.tocsr(), matrix_valid.tocsr(), matrix_input_with_negative.tocsr()
+        return result, matrix_input.tocsr(), matrix_test.tocsr(), matrix_input_with_negative.tocsr()
 
-def entropy(matrix_train, matrix_valid, topk, total_steps,
-            retrain_interval, embedded_matrix=np.empty((0)), iteration=100,
+def entropy(matrix_train, matrix_test, rec_model, topk, test_index, total_steps,
+            latent, embedded_matrix=np.empty((0)), iteration=100,
             rank=200, corruption=0.2, gpu_on=True, lam=80, optimizer="RMSProp",
             beta=1.0, **unused):
 
@@ -95,8 +95,6 @@ def entropy(matrix_train, matrix_valid, topk, total_steps,
     matrix_input = matrix_train
     if embedded_matrix.shape[0] > 0:
         matrix_input = vstack((matrix_input, embedded_matrix.T))
-
-    matrix_input_with_negative = matrix_input.copy()
 
     m, n = matrix_input.shape
 
@@ -111,7 +109,7 @@ def entropy(matrix_train, matrix_valid, topk, total_steps,
     for i in range(total_steps):
         print('This is step {} \n'.format(i))
         print('The number of nonzero in train set is {}'.format(len(matrix_input.nonzero()[0])))
-        print('The number of nonzero in valid set is {}'.format(len(matrix_valid.nonzero()[0])))
+        print('The number of nonzero in test set is {}'.format(len(matrix_test.nonzero()[0])))
 
         if i % retrain_interval == 0:
             progress.section("Training")
@@ -149,11 +147,11 @@ def entropy(matrix_train, matrix_valid, topk, total_steps,
             return metrics_result
 
         progress.section("Create Metrics")
-        result = entropy_selection.eval(prediction_scores, matrix_input, matrix_valid, topk, gpu_on)
+        result = entropy_selection.eval(prediction_scores, matrix_input, matrix_test, topk, gpu_on)
 
 
         progress.section("Update Train Set and Valid Set Based On Sampling Results")
-        result, matrix_input, matrix_valid, matrix_input_with_negative = entropy_selection.update_matrix(prediction, matrix_valid, result, matrix_input, matrix_input_with_negative)
+        result, matrix_input, matrix_test, matrix_input_with_negative = entropy_selection.update_matrix(prediction, matrix_test, result, matrix_input, matrix_input_with_negative)
 
         metrics_result.append(result)
 
