2f77949ff1c1e408fe5d594168a41894a478b4fd
k9luo <lucas.luokai@gmail.com>
2019-02-08 19:18:10 -0500

    sync some minor syntax in random

--- active_learning_models/random.py
+++ active_learning_models/random.py
@@ -30,7 +30,7 @@ class Random(object):
         print('The number of zeros predicted is {}'.format(len(prediction_test_zeros_intersect)))
 
         result['Num_Ones_In_Train'] = len(matrix_input.nonzero()[0])
-        result['Num_Ones_In_Valid'] = len(matrix_test.nonzero()[0])
+        result['Num_Ones_In_Test'] = len(matrix_test.nonzero()[0])
         result['Num_Ones_In_Prediction'] = len(prediction_test_ones_intersect)
         result['Num_Zeros_In_Prediction'] = len(prediction_test_zeros_intersect)
 
@@ -77,7 +77,7 @@ def random(matrix_train, matrix_test, topk, test_index, total_steps,
         progress.section("Create Metrics")
         result = eval(matrix_test[:test_index], topk, prediction)
 
-        progress.section("Update Train Set and Valid Set Based On Sampling Results")
+        progress.section("Update Train Set and Test Set Based On Sampling Results")
         result, matrix_input = random_selection.update_matrix(prediction, matrix_test, matrix_input, result, test_index)
 
         metrics_result.append(result)
